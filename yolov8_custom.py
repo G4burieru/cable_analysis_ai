de4efae10694fd2d9e7b3152b02757407aaa1ad8
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2023-11-29 11:05:14 -0300

    added centroid coords on image

--- yolov8_custom.py
+++ yolov8_custom.py
@@ -12,6 +12,10 @@ class ObjectDetection:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print(f'Using {self.device} device')
         self.counter = 0
+        # font 
+        self.font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        # fontScale 
+        self.fontScale = 1
     
     def load_model(self):
         
@@ -35,6 +39,7 @@ class ObjectDetection:
             boxes = r.boxes.cpu().numpy()
         
             xyxys.append(boxes.xyxy)
+            confidences.append(boxes.conf)
             
             if len(boxes.xyxy) > 0:
                 for xyxy in xyxys:
@@ -42,13 +47,15 @@ class ObjectDetection:
                     center_x = int((xyxy[0][0] + xyxy[0][2])/2)
                     center_y = int((xyxy[0][1] + xyxy[0][3])/2)
                     cv2.circle(image, (center_x, center_y), radius=0, color=(5, 94, 255), thickness=5)
+                    cv2.rectangle(image, (int(xyxy[0][0]), int(xyxy[0][1])-25), (int(xyxy[0][0])+100, int(xyxy[0][1])), (5, 94, 255), -1)
+                    cv2.putText(image, f"{center_x}, {center_y}", (int(xyxy[0][0]), int(xyxy[0][1])-6), self.font, self.fontScale, (255, 255, 255), 2, cv2.LINE_AA) 
+                    
                 
             path = f'centroids/{self.counter}.jpg'
             print(path)
             self.counter += 1
             cv2.imwrite(path, image) 
             
-            confidences.append(boxes.conf)
         
         return image, xyxys, confidences
     
