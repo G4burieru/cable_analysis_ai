c6375161e024d740abb94b3972e0f2ae78be7aae
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2023-12-05 18:40:13 -0300

    added distance between centroids

--- yolov8_custom.py
+++ yolov8_custom.py
@@ -16,6 +16,10 @@ class ObjectDetection:
         self.font = cv2.FONT_HERSHEY_COMPLEX_SMALL
         # fontScale 
         self.fontScale = 1
+        self.safety_area_xy1 = (0, 200)
+        self.safety_area_xy2 = (640, 350)
+        self.center_safety_x = int((self.safety_area_xy1[0] + self.safety_area_xy2[0])/2)
+        self.center_safety_y = int((self.safety_area_xy1[1] + self.safety_area_xy2[1])/2)
     
     def load_model(self):
         
@@ -47,11 +51,16 @@ class ObjectDetection:
                     center_x = int((xyxy[0][0] + xyxy[0][2])/2)
                     center_y = int((xyxy[0][1] + xyxy[0][3])/2)
                     cv2.circle(image, (center_x, center_y), radius=0, color=(5, 94, 255), thickness=5)
+
                     cv2.rectangle(image, (int(xyxy[0][0]), int(xyxy[0][1])-25), (int(xyxy[0][0])+100, int(xyxy[0][1])), (5, 94, 255), -1)
                     cv2.putText(image, f"{center_x}, {center_y}", (int(xyxy[0][0]), int(xyxy[0][1])-6), self.font, self.fontScale, (255, 255, 255), 2, cv2.LINE_AA) 
                     
+                    distance_y = center_y - self.center_safety_y
+                    cv2.rectangle(image, (int(xyxy[0][0]), int(xyxy[0][3])), (int(xyxy[0][0])+300, int(xyxy[0][3])+25), (5, 94, 255), -1)
+                    cv2.putText(image, f"distance in y = {distance_y} px", (int(xyxy[0][0]+10), int(xyxy[0][3])+20), self.font, self.fontScale, (255, 255, 255), 2, cv2.LINE_AA)
+                    
                 
-            cv2.rectangle(image, (0, 200), (640, 350), (0, 255, 0), 1)
+            cv2.rectangle(image, self.safety_area_xy1, self.safety_area_xy2, (0, 255, 0), 1)
             path = f'centroids/{self.counter}.jpg'
             print(path)
             self.counter += 1
